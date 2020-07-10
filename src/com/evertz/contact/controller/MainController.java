38976a9d81cd1a04b84441d23e64f3d4062b2f15
supreeths8 <supreeths8@gmail.com>
2020-07-10 20:54:58 +0530

    for angular

--- src/com/evertz/contact/controller/MainController.java
+++ src/com/evertz/contact/controller/MainController.java
@@ -12,7 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.evertz.contact.model.Admin;
@@ -28,7 +31,8 @@ import com.evertz.contact.model.Contact;
 import com.evertz.contact.service.BalanceService;
 import com.evertz.contact.service.ContactService;
 
-@Controller
+@CrossOrigin("*")
+@RestController
 public class MainController {
 
 	@Autowired
@@ -67,7 +71,7 @@ public class MainController {
 	}
 
 ////////////////////////////////////////////////////////////////////////////
-	@RequestMapping(value = "/index")
+	@GetMapping("/index")
 	public ResponseEntity<List<Contact>> index() {
 		List<Contact> list = service.listAll();
 		return ResponseEntity.ok().body(list);
